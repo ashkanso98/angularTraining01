4e3621cdf24235e11524eeb6f50fd22a5fda64da
ashkanso98 <ashkan9877so@gmail.com>
2025-07-12 12:02:22 -0700

    session 34 added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,7 +9,11 @@ import {
   output,
 } from '@angular/core';
 // import { DUMMY_USERS } from '../dummy-users';
-
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 // const rand = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
@@ -19,16 +23,22 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  // @Input({ required: true }) user!: {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // };
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
   // @Output() select = new EventEmitter();
   select = output<string>();
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
   // selectedUser = signal(DUMMY_USERS[rand]);
   // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
